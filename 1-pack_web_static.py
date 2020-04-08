e01b6ca752a5cad44a2426f475138367d57c0630
Cristian <1154@holbertonschool.com>
2020-04-08 00:57:36 +0000

    Fabric convert tagz files

---
+++ 1-pack_web_static.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Compress files"""
+from fabric.api import *
+from datetime import datetime
+from os.path import getsize
+
+
+def do_pack():
+    """Function that compress a files"""
+    fil = "web_static_{:s}.tgz".format(datetime.now().strftime("%Y%m%d%H%M%S"))
+    print("Packing web_static to versions/{}".format(fil))
+
+    local("mkdir -p versions")
+    path = local("tar -cvzf versions/{:s} web_static".format(fil))
+
+    size = getsize("versions/{}".format(fil))
+
+    if path.succeeded:
+        print("web_static packed: versions/{} -> {}Bytes".format(fil, size))
+    else:
+        return None
