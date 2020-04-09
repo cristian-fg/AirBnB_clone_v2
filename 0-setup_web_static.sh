80f4ad386e96ecba792a897b49513b7ec50b491c
Cristian <1154@holbertonschool.com>
2020-04-09 19:48:57 +0000

    Updating task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -10,11 +10,9 @@ sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 sudo touch /data/web_static/releases/test/index.html
 echo -e "<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>" | sudo tee /data/web_static/releases/test/index.html
-
-sed -i "s/root \/var\/www\/html;/root \/data\/web_static\/current\/;/g" /etc/nginx/sites-enabled/default
-sed -i "/root \/data\/web_static\/current\/;/a \ \n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-enabled/default
-
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -sfn /data/web_static/releases/test/ /data/web_static/current/
 sudo chown -R ubuntu:ubuntu /data/
+
+sudo sed -i "43 a \ \n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-enabled/default
 
 sudo service nginx restart
