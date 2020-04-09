9c76d8ba4e96523e3aefada74f763561928955d6
Cristian <1154@holbertonschool.com>
2020-04-09 19:20:33 +0000

    Updating task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -16,19 +16,19 @@ def do_deploy(archive_path):
         # ['web_static_20170315003959', 'tgz']
 
         put(archive_path, "/tmp/")
-        sudo("mkdir -p /data/web_static/releases/{}".format(p2[0]))
+        sudo("mkdir -p /data/web_static/releases/{}/".format(p2[0]))
 
-        sudo("tar -xzf /tmp/{} -C /data/web_static/releas\
-            es/{}".format(p1[-1], p2[0]))
+        data = "/data/web_static/releases"
+        sudo("tar -xzf /tmp/{} -C {}/{}/".format(p1[-1], data, p2[0]))
         sudo("rm /tmp/{}".format(p1[-1]))
 
-        sudo("mv /data/web_static/releases/{0}/web_static/* /data/web_static/release\
-            s/{0}/".format(p2[0]))
+        path = "mv /data/web_static/releases"
+        path2 = "/data/web_static/releases"
+        sudo("{}/{}/web_static/* {}/{}/".format(path, p2[0], path2, p2[0]))
 
-        sudo("rm -rf /data/web_static/releases/{}/web_static/".format(p2[0]))
+        sudo("rm -rf {}/{}//web_static".format(path2, p2[0]))
         sudo("rm -rf /data/web_static/current")
-        sudo("ln -s /data/web_static/releases/{} /data/web_stat\
-            ic/current".format(p2[0]))
+        sudo("ln -s {}/{}/ /data/web_static/current".format(path2, p2[0]))
 
         print("New version deployed!")
         return True
