bf3a864cb40e875d73818e9dfdeb967e17f4e19d
Cristian <1154@holbertonschool.com>
2020-04-10 17:28:49 +0000

    Add task 3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,20 +1,68 @@
 #!/usr/bin/python3
 """distributes an archive to your web servers"""
 from fabric.api import *
-
-pack = __import__('1-pack_web_static')
-up = __import__('2-do_deploy_web_static')
+from datetime import datetime
+import os.path
 
 
 env.hosts = ["35.231.133.57", "54.91.43.217"]
 
 
+@runs_once
+def do_pack():
+    """Function that compress a files"""
+    fil = "web_static_{:s}.tgz".format(datetime.now().strftime("%Y%m%d%H%M%S"))
+    print("Packing web_static to versions/{}".format(fil))
+
+    local("mkdir -p versions")
+    path = local("tar -cvzf versions/{:s} web_static".format(fil))
+
+    size = getsize("versions/{}".format(fil))
+
+    if path.succeeded:
+        print("web_static packed: versions/{} -> {}Bytes".format(fil, size))
+        return("versions/{}".format(fil))
+    else:
+        return None
+
+
+def do_deploy(archive_path):
+    """functio that distributes an archive to your web servers"""
+    if os.path.isfile(archive_path):
+        p1 = archive_path.split("/")
+        # ['versions', 'web_static_20170315003959.tgz']
+        p2 = p1[-1].split(".")
+        # ['web_static_20170315003959', 'tgz']
+
+        put(archive_path, "/tmp/")
+        sudo("mkdir -p /data/web_static/releases/{}/".format(p2[0]))
+
+        data = "/data/web_static/releases"
+        sudo("tar -xzf /tmp/{} -C {}/{}/".format(p1[-1], data, p2[0]))
+        sudo("rm /tmp/{}".format(p1[-1]))
+
+        path = "mv /data/web_static/releases"
+        path2 = "/data/web_static/releases"
+        sudo("{}/{}/web_static/* {}/{}/".format(path, p2[0], path2, p2[0]))
+
+        sudo("rm -rf {}/{}//web_static".format(path2, p2[0]))
+        sudo("rm -rf /data/web_static/current")
+        sudo("ln -s {}/{}/ /data/web_static/current".format(path2, p2[0]))
+
+        print("New version deployed!")
+        return True
+
+    else:
+        return False
+
+
+@task
 def deploy():
     """Deploy all automatic"""
 
-    new = pack.do_pack()
+    new = do_pack()
     if new is None:
         return False
 
-    new_up = up.do_deploy(new)
+    new_up = do_deploy(new)
     return new_up
